0ed83c8fcdf7b056afb12e78445307009ca1ebbb
mico <mic0@mic0.dev>
2026-03-24 17:13:43 +0100

    mend
    Multi faucet minting merge

--- crates/zoroswap/src/lib.rs
+++ crates/zoroswap/src/lib.rs
@@ -5,7 +5,6 @@ pub mod notes_listener;
 pub mod oracle_sse;
 pub mod order;
 pub mod server;
-pub mod sqlite;
 pub mod trading_engine;
 pub mod websocket;
 
@@ -15,7 +14,6 @@ pub use faucet::*;
 pub use oracle_sse::*;
 pub use order::*;
 pub use server::*;
-pub use sqlite::*;
 pub use trading_engine::*;
 pub use zoro_curve_base::*;
 
